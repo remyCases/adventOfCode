cda722cb73530b69870f72c23f1ce3993c2bf67e
Rémy <remy.cases@gmail.com>
2023-01-18 13:57:46 +0100

    add second part of day 3

--- 2022/src/dayThree.nim
+++ 2022/src/dayThree.nim
@@ -10,6 +10,9 @@ const upperLetter: HashSet[char] = collect(initHashSet()):
 const firstLowerLetterHex = 0x61
 const firstUpperLetterHex = 0x41
 
+const COUNTER_MAX = 3
+var counter = 0
+
 proc computePriority(intersectContent: HashSet[char]): int =
     var priority = 0
     for i in intersectContent * lowerLetter:
@@ -18,31 +21,55 @@ proc computePriority(intersectContent: HashSet[char]): int =
         priority += int(i) - firstUpperLetterHex + 27
     return priority
 
-proc parse(line: string, firstRucksacksContent, secondRucksacksContent: var HashSet[char]) =
+proc computePriorityEveryThird(intersectContent: HashSet[char]): int =
+    if counter < COUNTER_MAX: return 0
+    var priority = 0
+    for i in intersectContent * lowerLetter:
+        priority += int(i) - firstLowerLetterHex + 1
+    for i in intersectContent * upperLetter:
+        priority += int(i) - firstUpperLetterHex + 27
+    return priority
+
+proc parseInTwoContents(line: string, intersect: var HashSet[char]) =
     if line.len == 0: return
     let halfLineLen: int = euclDiv[int](line.len, 2)
 
-    firstRucksacksContent = line[0..<halfLineLen].toHashSet()
-    secondRucksacksContent = line[halfLineLen..^1].toHashSet()
+    let firstRucksacksContent = line[0..<halfLineLen].toHashSet()
+    let secondRucksacksContent = line[halfLineLen..^1].toHashSet()
+    intersect = firstRucksacksContent * secondRucksacksContent
 
+proc parseInOneContents(line: string, intersect: var HashSet[char]) =
+    if line.len == 0: return
+    if counter >= COUNTER_MAX:
+        counter = 0
+        intersect = line.toHashSet()
+    else:
+        intersect = intersect * line.toHashSet()
+    counter.inc
 
 proc readFileAndComputePriority(filename: string, option = '1') =
     var priority = 0
-    var firstRucksacksContent = HashSet[char]()
-    var secondRucksacksContent = HashSet[char]()
+    var intersect = collect(initHashSet()):
+        for i in {'a' .. 'z', 'A' .. 'Z'}: {i}
 
     let procCompute = case option:
     of '1':
         computePriority
     of '2':
-        computePriority
+        computePriorityEveryThird
+    else:
+        return
+
+    let parse = case option:
+    of '1':
+        parseInTwoContents
+    of '2':
+        parseInOneContents
     else:
         return
 
     for line in filename.lines:
-        parse(line, firstRucksacksContent, secondRucksacksContent)
-        let intersect =  firstRucksacksContent * secondRucksacksContent
-
+        parse(line, intersect)
         priority += procCompute(intersect)
     echo priority
 
