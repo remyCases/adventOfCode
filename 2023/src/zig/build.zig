cdfc8cd3b38e70f772e8a89e8c7f18718591db81
remyCases <remy.cases@gmail.com>
2024-11-07 00:46:14 +0100

    [Minor] fixes zig build and clap submodule*

--- 2023/src/zig/build.zig
+++ 2023/src/zig/build.zig
@@ -1,12 +1,11 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void 
-{
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
-	.name = "mainZig",
-	.root_source_file = .{ .path = "mainZig.zig" },
+        .name = "mainZig",
+        .root_source_file = b.path("mainZig.zig"),
         .target = target,
         .optimize = optimize,
     });
