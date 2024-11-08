32811cce46f279376632f2df8325bcbc4f92d944
remyCases <remy.cases@gmail.com>
2024-11-08 15:06:04 +0100

    [Minor] adds zig first part of first day

--- 2023/src/zig/mainZig.zig
+++ 2023/src/zig/mainZig.zig
@@ -1,28 +1,40 @@
 const std = @import("std");
+const clap = @import("clap");
+const dayOne = @import("dayOne.zig");
 
-pub fn main() !void 
-{
-    var out = std.io.getStdOut().writer();
-    
+pub fn main() !void {
     // get general allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    // parse args into string array
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
+    // clap elements
+    const params = comptime clap.parseParamsComptime(
+        \\-d, --day <usize>      An option parameter, which takes a value.
+        \\-p, --part <usize>     An option parameter which can be specified multiple times.
+    );
 
-    for(args) |arg| 
-    {
-        if (std.mem.eql(u8, arg, "--day"))
-        {
-            
-        }
-        else if (std.mem.eql(u8, arg, "--part"))
-        {
+    var diag = clap.Diagnostic{};
+    var res = clap.parse(clap.Help, &params, clap.parsers.default, .{
+        .diagnostic = &diag,
+        .allocator = allocator,
+    }) catch |err| {
+        diag.report(std.io.getStdErr().writer(), err) catch {};
+        return err;
+    };
+    defer res.deinit();
 
+    // resolve parsed arguments
+    if (res.args.day) |d| {
+        if (res.args.part) |p| {
+            switch (d) {
+                1 => try dayOne.main(p),
+                else => return error.InvalidInput,
+            }
+        } else {
+            return error.NullInput;
         }
-        try out.print(" {s}\n", .{arg});
+    } else {
+        return error.NullInput;
     }
 }
