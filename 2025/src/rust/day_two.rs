5365d97e368752d84344dcfe50360f059902967e
Remy <remy.cases@gmail.com>
2025-12-06 13:49:49 +0100

    [Minor] attempt for 2025-2-1 rust

--- 2025/src/rust/day_two.rs
+++ 2025/src/rust/day_two.rs
@@ -9,33 +9,46 @@ use nom::*;
 use nom::error::Error as NomError;
 use aoc_utils::*;
 
+fn find_invalid_ids(s: u64, e: u64) -> u64 {
+    let n: u32 = s.ilog10();
+    if n % 2 == 0 { return 0; }
+
+    let u: u64 = 10u64.pow(n/2 + 1) + 1;
+    let us = if s % u == 0 { s / u } else { s / u + 1 };
+    let ue = e / u;
+    if ue < us { return 0; }
+    return ((us+1)*us / 2 - (ue-1)*ue / 2) * u;
+}
+
 fn read_file_and_compute(file_path: &Path, part: argparse::ArgPart) -> io::Result<()>
 {
-    let mut invalid_ids: u32 = 0;
+    let mut invalid_ids: u64 = 0;
 
     parse_compute!(
         file_path, part,
         multi::separated_list0(
             character::complete::char::<&str, NomError<_>>(','),
             sequence::separated_pair(
-                combinator::map_res(character::complete::digit1, str::parse),
+                combinator::map_res(character::complete::digit1, str::parse::<u64>),
                 character::complete::char('-'),
                 combinator::map_res(character::complete::digit1, str::parse),
             )
         ), 
-        |v: Vec<(u32, u32)>| {
-            for (s,e) in v {
-                let ns: u32 = s.ilog10();
+        |v: Vec<(u64, u64)>| {
+            for (mut s,e) in v {
+                let mut ns: u32 = s.ilog10();
                 let ne: u32 = e.ilog10();
-                if ne == ns && ne % 2 == 0 { continue; }
-                for i in s..=e {
-                    let n: u32 = i.ilog10();
-                    if n % 2 == 0 { continue; }
-                    if i % ( 10u32.pow(n/2+1) + 1 ) == 0 { invalid_ids += i; println!("{}", i); }
+                while ns < ne
+                {
+                    invalid_ids += find_invalid_ids(s, 10u64.pow(ns+1)-1);
+                    ns += 1;
+                    s = 10u64.pow(ns);
                 }
+                invalid_ids += find_invalid_ids(s, e);
+
             }
         },
-        |v: Vec<(u32, u32)>| {
+        |v: Vec<(u64, u64)>| {
         }
     );
 
