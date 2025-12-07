2549e66ef4337b5e9198ec01c41b435a3dd056d2
remyCases <remy.cases@gmail.com>
2025-12-07 04:04:42 +0100

    [Minor] improvement 2025-3-2 rust

--- 2025/src/rust/day_three.rs
+++ 2025/src/rust/day_three.rs
@@ -9,10 +9,12 @@ use nom::*;
 use nom::error::Error as NomError;
 use aoc_utils::*;
 
+const NUMBER_BATTERIES: usize = 12;
+
 fn read_file_and_compute(file_path: &Path, part: argparse::ArgPart) -> io::Result<()>
 {
-    let mut batteries: [u32; 12] = [0; 12];
-    let mut indeces: [usize; 12] = [0; 12];
+    let mut batteries: [u32; NUMBER_BATTERIES] = [0; NUMBER_BATTERIES];
+    let mut indeces: [usize; NUMBER_BATTERIES] = [0; NUMBER_BATTERIES];
     let mut sum_joltage: u64 = 0;
 
     parse_compute!(
@@ -24,6 +26,8 @@ fn read_file_and_compute(file_path: &Path, part: argparse::ArgPart) -> io::Resul
             batteries[0] = 0;
             indeces[0] = 0;
             batteries[1] = 0;
+            indeces[1] = 0;
+
             for i in indeces[0]..(v.len()-1) {
                 if v[i] > batteries[0] {
                     batteries[0] = v[i];
@@ -39,16 +43,16 @@ fn read_file_and_compute(file_path: &Path, part: argparse::ArgPart) -> io::Resul
         },
         |v: Vec<u32>| {
             let mut joltage: u64 = 0;
-            for i in 0..12 {
+            for i in 0..NUMBER_BATTERIES {
                 batteries[i] = 0;
                 indeces[i] = 0;
             }
 
-            for i in 0..12 {
-                for j in indeces[i]..(v.len()-11+i) {
+            for i in 0..NUMBER_BATTERIES {
+                for j in indeces[i]..(v.len()-NUMBER_BATTERIES+1+i) {
                     if v[j] > batteries[i] {
                         batteries[i] = v[j];
-                        if i != 11 { indeces[i+1] = j+1; }
+                        if i != NUMBER_BATTERIES-1 { indeces[i+1] = j+1; }
                     }
                 }
                 joltage = joltage * 10 + batteries[i] as u64;
