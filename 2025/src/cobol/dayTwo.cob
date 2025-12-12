7caaef6633043fa042a751602afef094bab5a12f
remyCases <remy.cases@gmail.com>
2025-12-12 01:22:59 +0100

    [Minor] 2025-2 cobol

--- 2025/src/cobol/dayTwo.cob
+++ 2025/src/cobol/dayTwo.cob
@@ -21,22 +21,38 @@
        01 FData PIC X(1).
 
        WORKING-STORAGE SECTION.
+       *> text parsing
        01 WSEOF PIC A(1).
        01 WSFileStatus PIC 99.
        01 WSFieldIndex PIC 999 VALUE 1.
        01 WSCurrentData PIC X(50).
+       *> variables for the start of a range
        01 WSStart PIC 9(18).
        01 WSStartDiv PIC 9(18).
+       01 WSStartZeros PIC 9(3).
+       01 WSStartSize PIC 9(3).
+       *> variables for the end of a range
+       01 WSEnd PIC 9(18).
+       01 WSEndZeros PIC 9(3).
        01 WSRangeEnd PIC 9(18).
        01 WSEndDiv PIC 9(18).
-       01 WSDivisor PIC 9(18).
-       01 Tmp PIC 9(18).
-       01 WSEnd PIC 9(18).
-       01 WSStartZeros PIC 9(3).
-       01 WSEndZeros PIC 9(3).
-       01 WSRangeInvalidIds PIC 9(18).
+       *> variables for the invalid ids
+       01 WSFoundIds.
+           05 WSFoundCount PIC 9(5) COMP VALUE 0.
+           05 WSFoundVals PIC 9(18) OCCURS 1000 TIMES.
+       01 WSFound  PIC 9 COMP VALUE 0.
+       01 WSInvalidId PIC 9(18).
        01 WSInvalidIds PIC 9(18).
        01 WSInvalidIdsDisplay PIC Z(17)9.
+       *> variables for the divisors
+       01 WSDivisorStr PIC X(18).
+       01 WSDivisor PIC 9(18).
+       01 WSDiv PIC 9(18).
+       *> tmp variables
+       01 Tmp PIC 9(18).
+       *> counters
+       01 I PIC 9(5) COMP VALUE 0.
+       01 J PIC 9(5) COMP VALUE 0.
 
        LINKAGE SECTION.
        01 LPart PIC 9 VALUE 1.
@@ -137,13 +153,66 @@
                NEXT SENTENCE
            END-IF
 
-           MOVE 0 TO WSRangeInvalidIds
-           COMPUTE Tmp = WSEndDiv * (WSEndDiv + 1) / 2
-           ADD Tmp TO WSRangeInvalidIds
-           COMPUTE Tmp = WSStartDiv * (WSStartDiv - 1) / 2
-           SUBTRACT Tmp FROM WSRangeInvalidIds
-           MULTIPLY WSDivisor BY WSRangeInvalidIds
-
-           ADD WSRangeInvalidIds TO WSInvalidIds.
+           MOVE WSStartDiv TO I
+           PERFORM UNTIL I GREATER THAN WSEndDiv
+               COMPUTE WSInvalidId = I * WSDivisor
+               PERFORM AddUniqueId
+               ADD 1 TO I
+           END-PERFORM.
 
        FindRepeatedIds.
+           MOVE 2 TO WSDiv
+           COMPUTE WSStartSize = 18 - WSStartZeros
+           PERFORM UNTIL WSDiv GREATER THAN WSStartSize
+               PERFORM FindRepeatedIdWithRepetition
+               ADD 1 TO WSDiv
+           END-PERFORM.
+
+       FindRepeatedIdWithRepetition.
+           COMPUTE Tmp = FUNCTION MOD(WSStartSize, WSDiv)
+           IF Tmp NOT EQUAL TO 0
+               NEXT SENTENCE
+           END-IF
+    
+           MOVE WSDiv TO I
+           MOVE ZEROES TO WSDivisorStr
+           PERFORM UNTIL I EQUALS TO 0
+               COMPUTE Tmp = 18 - WSStartSize / WSDiv * (I - 1)
+               MOVE 1 TO WSDivisorStr(Tmp:1)
+               SUBTRACT 1 FROM I
+           END-PERFORM
+           MOVE WSDivisorStr TO WSDivisor
+
+           COMPUTE Tmp = FUNCTION MOD(WSStart, WSDivisor)
+           IF Tmp EQUAL TO 0
+               COMPUTE WSStartDiv = WSStart / WSDivisor
+           ELSE
+               COMPUTE WSStartDiv = WSStart / WSDivisor + 1
+           END-IF
+           COMPUTE WSEndDiv = WSRangeEnd / WSDivisor
+
+           MOVE WSStartDiv TO I
+           PERFORM UNTIL I GREATER THAN WSEndDiv
+               COMPUTE WSInvalidId = I * WSDivisor
+               PERFORM AddUniqueId
+               ADD 1 TO I
+           END-PERFORM.
+
+       AddUniqueId.
+           MOVE 0 TO WSFound
+           MOVE 1 TO J
+
+           PERFORM UNTIL J GREATER THAN WSFoundCount 
+           OR WSFound EQUALS TO 1
+               MOVE WSFoundVals(J) TO Tmp
+               IF Tmp EQUALS TO WSInvalidId
+                   MOVE 1 TO WSFound
+               END-IF
+               ADD 1 TO J
+           END-PERFORM
+
+           IF WSFound EQUALS TO 0
+               ADD 1 TO WSFoundCount
+               ADD WSInvalidId TO WSInvalidIds
+               MOVE WSInvalidId TO WSFoundVals(WSFoundCount)
+           END-IF.
